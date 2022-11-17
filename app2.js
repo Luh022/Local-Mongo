c46442af4a6ab1a6db83610145ef13fe61f7947f
Luana Roza <102182950+Luh022@users.noreply.github.com>
2022-11-17 15:57:50 -0300

    Add files via upload

---
+++ app2.js
@@ -0,0 +1,25 @@
+
+
+import React from "react";
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
+import {NavigationContainer} from "@react-navigation/native";
+import SignIn from './screens/SignIn';
+import SignUp from "./screens/SignUp";
+import {AuthProvider} from "./context/auth";
+import Home from "./screens/Home";
+
+const Stack = createNativeStackNavigator();
+
+export default function App () {
+  return (
+    <NavigationContainer>
+      <AuthProvider>
+        <Stack.Navigator initialRouteName="SignIn">
+          <Stack.Screen name="SignUp" component={SignUp} />
+          <Stack.Screen name="SignIn" component={SignIn} />
+          <Stack.Screen name="Home" component={Home} />
+        </Stack.Navigator>
+      </AuthProvider>
+    </NavigationContainer>
+  )
+}
